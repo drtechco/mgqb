a6447aa6660f273f27182c598cdb80208e098b3e
none <none@none.com>
2022-06-27 22:35:47 +0700

    setWindowFields finish

--- addFields.go
+++ addFields.go
@@ -1,6 +1,9 @@
 package mgqb
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 type addFields struct {
 	fields map[string]interface{}
@@ -51,7 +54,7 @@ func (s *addFields) Replace(field string, collField string, documents ...interfa
 	return s
 }
 
-func (s *addFields) WindowOperator(field string, operator operator, operatorPars string) {
+func (s *addFields) WindowOperator(field string, operator operator, operatorPars string) *addFields {
 	if d, ok := s.fields[field].(bson.D); ok {
 		if e, ok := findDWithE(d, string(operator)); ok {
 			e.Value = operatorPars
@@ -62,13 +65,77 @@ func (s *addFields) WindowOperator(field string, operator operator, operatorPars
 			})
 		}
 	}
+	return s
+}
+
+func (s *addFields) Rang(field string, rangeVal ...interface{}) *addFields {
+	s.checkField(field, "window", bson.D{{"range", rangeVal}}, func(w, a bson.D) interface{} {
+		if a, ok := findDWithE(w, "range"); ok {
+			if a1, ok := a.Value.([]interface{}); ok {
+				return append(a1, rangeVal...)
+			} else {
+				panic(errors.New("range value is not slice"))
+			}
+		} else {
+			return a
+		}
+	})
+	return s
+}
+
+func (s *addFields) Unit(field string, unit interface{}) *addFields {
+	s.checkField(field, "window", bson.D{{"unit", unit}}, func(e, a bson.D) interface{} {
+		return unit
+	})
+	return s
+}
+
+func (s addFields) checkField(field string, subField string, addVal bson.D, setFun func(exists, a bson.D) interface{}) {
+	if f, ok := s.fields[field]; ok {
+		if d, ok := f.(bson.D); ok {
+			if wi, ok := findDWithE(d, subField); ok {
+				if w, ok := wi.Value.(bson.D); ok {
+					wi.Value = setFun(w, addVal)
+				} else {
+					panic(errors.New("documents is not bson.E"))
+				}
+			} else {
+				s.fields[field] = append(d, bson.E{
+					Key:   subField,
+					Value: addVal,
+				})
+			}
+		} else {
+			panic(errors.New("field is not bson.D"))
+		}
+	} else {
+		panic(errors.New("field is nil"))
+	}
 }
 
 func (s *addFields) setDoc(field string, documents ...interface{}) {
 	if len(documents) > 0 {
-		s.fields[field] = append(s.fields[field].(bson.D), bson.E{
-			Key:   "window",
-			Value: bson.E{Key: "documents", Value: documents},
+		s.checkField(field, "window", bson.D{{"documents", documents}}, func(w, a bson.D) interface{} {
+			if a, ok := findDWithE(w, "range"); ok {
+				if a1, ok := a.Value.([]interface{}); ok {
+					return append(a1, documents...)
+				} else {
+					panic(errors.New("documents value is not slice"))
+				}
+			} else {
+				return a
+			}
 		})
 	}
+}
+
+func (s *addFields) ES() []bson.E {
+	res := make([]bson.E, 0)
+	for key, f := range s.fields {
+		res = append(res, bson.E{
+			Key:   key,
+			Value: f,
+		})
+	}
+	return res
 }
--- setWindowFields.go
+++ setWindowFields.go
@@ -1,32 +1,76 @@
 package mgqb
 
 import "go.mongodb.org/mongo-driver/bson"
+
 type setWindowFields struct {
-	partitionBy string
-	sortBy      bson.M
+	partitionBy interface{}
+	sortBy      bson.D
+	output      []*addFields
+	outputRaw   bson.D
 }
 
 func SetWindowFields() *setWindowFields {
 	return &setWindowFields{
-		sortBy: make(bson.M),
+		sortBy:    make(bson.D, 0),
+		output:    make([]*addFields, 0),
+		outputRaw: make(bson.D, 0),
 	}
 }
 
-func (s *setWindowFields) PartitionBy(expression string) *setWindowFields {
+func (s *setWindowFields) PartitionByField(field string) *setWindowFields {
+	s.partitionBy = field
+	return s
+}
+
+func (s *setWindowFields) PartitionBySimple(field string, collField string) *setWindowFields {
+	s.partitionBy = bson.D{{field, collField}}
+	return s
+}
+
+func (s *setWindowFields) PartitionBy(expression bson.D) *setWindowFields {
 	s.partitionBy = expression
 	return s
 }
 
 func (s *setWindowFields) SortDesc(field string) *setWindowFields {
-	s.sortBy[field] = -1
+	if ex, ok := findDWithE(s.sortBy, field); ok {
+		ex.Value = -1
+	} else {
+		s.sortBy = append(s.sortBy, bson.E{Key: field, Value: -1})
+	}
 	return s
 }
 
 func (s *setWindowFields) SortAsc(field string) *setWindowFields {
-	s.sortBy[field] = 1
+	if ex, ok := findDWithE(s.sortBy, field); ok {
+		ex.Value = 1
+	} else {
+		s.sortBy = append(s.sortBy, bson.E{Key: field, Value: 1})
+	}
 	return s
 }
 
-func (s *setWindowFields) Output() {
-	
+func (s *setWindowFields) AddOutput(field *addFields) {
+	s.output = append(s.output, field)
+}
+
+func (s *setWindowFields) D() bson.D {
+	res := make(bson.D, 0)
+	if s.partitionBy != nil {
+		res = append(res, bson.E{Key: "partitionBy", Value: s.partitionBy})
+	}
+	if len(s.sortBy) > 0 {
+		res = append(res, bson.E{Key: "sortBy", Value: s.sortBy})
+	}
+	if len(s.output) > 0 {
+		for _, o := range s.output {
+			for _, e := range o.ES() {
+				s.outputRaw = append(s.outputRaw, e)
+			}
+		}
+	}
+	if len(s.outputRaw) > 0 {
+		res = append(res, bson.E{Key: "output", Value: s.outputRaw})
+	}
+	return res
 }
